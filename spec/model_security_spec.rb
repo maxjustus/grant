3b84a4d1f4a2ad85bfe05b2ad77a50712996d578
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2010-02-16 19:14:21 -0500

    Updated specs

--- spec/model_security_spec.rb
+++ spec/model_security_spec.rb
@@ -10,22 +10,7 @@ describe Grant::ModelSecurity do
   
   describe 'module include' do
     it 'should establish failing ActiveRecord callbacks for before_create, before_update, before_destroy, and after_find when included' do
-      class TestModel; end
-      TestModel.stub!(:has_and_belongs_to_many)
-      TestModel.stub!(:has_many)
-      TestModel.should_receive(:before_create).with(:grant_before_create)
-      TestModel.should_receive(:before_update).with(:grant_before_update)
-      TestModel.should_receive(:before_destroy).with(:grant_before_destroy)
-      TestModel.should_receive(:after_find).with(:grant_after_find)
-      TestModel.instance_eval do
-        include Grant::ModelSecurity
-      end
-      instance = TestModel.new
-      instance.stub!(:id).and_return(1)
-      lambda { instance.grant_before_create }.should raise_error(Grant::Error)
-      lambda { instance.grant_before_update }.should raise_error(Grant::Error)
-      lambda { instance.grant_before_destroy }.should raise_error(Grant::Error)
-      lambda { instance.grant_after_find }.should raise_error(Grant::Error)
+      verify_standard_callbacks(new_model_class.new)
     end
     
     it 'should establish failing ActiveRecord callbacks for any has_many or has_and_belongs_to_many associations' do
@@ -44,25 +29,34 @@ describe Grant::ModelSecurity do
   
   describe '#grant' do
     it 'should allow after_find callback to succeed when granted' do
-      verify_standard_callbacks(:update)
+      c = new_model_class.instance_eval { grant(:find); self }
+      verify_standard_callbacks(c.new, :find)
     end
     
     it 'should allow before_create callback to succeed when granted' do
-      verify_standard_callbacks(:create)
+      c = new_model_class.instance_eval { grant(:create); self }
+      verify_standard_callbacks(c.new, :create)
     end
     
     it 'should allow before_update callback to succeed when granted' do
-      verify_standard_callbacks(:destroy)
+      c = new_model_class.instance_eval { grant(:update); self }
+      verify_standard_callbacks(c.new, :update)
     end
     
     it 'should allow before_destroy callback to succeed when granted' do
-      verify_standard_callbacks(:destroy)
+      c = new_model_class.instance_eval { grant(:destroy); self }
+      verify_standard_callbacks(c.new, :destroy)
     end
     
     it 'should allow multiple callbacks to be specified with one grant statment' do
-      verify_standard_callbacks(:create, :update)
-      verify_standard_callbacks(:create, :update, :destroy)
-      verify_standard_callbacks(:create, :update, :destroy, :find)
+      c = new_model_class.instance_eval { grant(:create, :update); self }
+      verify_standard_callbacks(c.new, :create, :update)
+      
+      c = new_model_class.instance_eval { grant(:create, :update, :destroy); self }
+      verify_standard_callbacks(c.new, :create, :update, :destroy)
+      
+      c = new_model_class.instance_eval { grant(:create, :update, :destroy, :find); self }
+      verify_standard_callbacks(c.new, :create, :update, :destroy, :find)
     end
     
     it 'should allow adding to an association to succeed when granted' do
@@ -92,34 +86,57 @@ describe Grant::ModelSecurity do
       lambda { instance.grant_add_groups(new_model_class) }.should raise_error(Grant::Error)
       lambda { instance.grant_remove_groups(new_model_class) }.should_not raise_error(Grant::Error)
     end
-    
-    def verify_standard_callbacks(*succeeding_callbacks)
-      succeeding_callbacks = Array(succeeding_callbacks)
-      c = new_model_class
-      c.instance_eval do
-        grant(*succeeding_callbacks)
-      end
-      instance = c.new
-      [:create, :update, :destroy, :find].each do |callback|
-        grant_callback = "grant_" + (callback == :find ? "after_" : "before_") + callback.to_s
-        expectation = succeeding_callbacks.include?(callback) ? :should_not : :should
-        lambda { instance.send(grant_callback.to_sym) }.send(expectation, raise_error(Grant::Error))
-      end
+  end
+  
+  def verify_standard_callbacks(instance, *succeeding_callbacks)
+    succeeding_callbacks = Array(succeeding_callbacks)
+    [:create, :update, :destroy, :find].each do |action|
+      expectation = succeeding_callbacks.include?(action) ? :should_not : :should
+      lambda { instance.send(action) }.send(expectation, raise_error(Grant::Error))
     end
   end
   
   def new_model_class
-    c = Class.new do
-      def id; 1 end
-      def self.has_and_belongs_to_many(association_id, options={}, &extension); end
-      def self.has_many(association_id, options={}, &extension); end
-      def self.before_create(*args); end
-      def self.before_update(*args); end
-      def self.before_destroy(*args); end
-      def self.after_find(*args); end
-    end
-    Class.new(c) do
+    Class.new(ActiveRecordMock) do
       include Grant::ModelSecurity
+    end
+  end
+  
+  class ActiveRecordMock
+    def id; 1 end
+    
+    def self.before_create(method)
+      define_method(:create) { send method }
+    end
+    
+    def self.before_update(method)
+      define_method(:update) { send method }
+    end
+    
+    def self.before_destroy(method)
+      define_method(:destroy) { send method }
+    end
+    
+    def self.after_find(method)
+      define_method(:find) { send method }
+    end
+    
+    def self.has_many(association_id, options, &extension)
+      define_method("add_#{association_id}".to_sym) do |associated_model|
+        Array(options[:before_add]).each { |callback| callback.call(associated_model) }
+      end
+      define_method("remove_#{association_id}".to_sym) do |associated_model|
+        Array(options[:before_remove]).each { |callback| callback.call(associated_model) }
+      end
+    end
+    
+    def self.has_and_belongs_to_many(association_id, options, &extension)
+      define_method("add_#{association_id}".to_sym) do |associated_model|
+        Array(options[:before_add]).each { |callback| callback.call(associated_model) }
+      end
+      define_method("remove_#{association_id}".to_sym) do |associated_model|
+        Array(options[:before_remove]).each { |callback| callback.call(associated_model) }
+      end
     end
   end
   
